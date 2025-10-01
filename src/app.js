76b4fea370ff00f56cd715c1edb8831c76955276
Abhay Kumar Pandey <abhaypandey7067@gmail.com>
2025-10-01 18:35:33 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,57 +2,28 @@ const express = require('express');
 
 const app = express();
 
-// app.use("/hello",(req,res) => {
-//     res.send( "Hello hello server"); // Request handler
-// });
+//We can send any number of route handlers as an array.
+//We can use it in any route method (get, post, put, delete, all, use).
+//Wrapping any number of route handlers in an array doesn't have any effect on the response.
+// app.use("/route", [rH1, rH2, rH3, ...]);
 
-app.get("/user", (req,res) => {
-    console.log(req.query);
-    res.send("Get user API called with query params");
-    // res.send({firstname: "John", lastname: "Doe"});
+app.use("/user", (req, res, next)=> {
+    next();
+    console.log("Route Handler");
+    // res.send("Response");
+}, (req, res, next)=> {
+    console.log("Route Handler 2");
+    // res.send("Response 2");
+    next();
+}, (req, res, next)=> {
+    console.log("Route Handler 3");
+    // res.send("Response 3");
+    next();
+}, (req, res, next)=> {
+    console.log("Route Handler 4");
+    res.send("Response 4");
+    next();
 });
-
-app.get("/user/:userID/:name/:password", (req,res) => {
-    console.log(req.params);
-    res.send("Get user API called with dynamic route");
-    // res.send({firstname: "John", lastname: "Doe"});
-});
-
-app.get("/user", (req,res) => {
-    console.log(res.params);
-    res.send("Get user API called");
-    // res.send({firstname: "John", lastname: "Doe"});
-});
-
-app.post("/user", (req,res) => {
-    res.send("Post user API called");
-    // res.send({firstname: "John", lastname: "Doe"});
-});
-
-app.put("/user", (req,res) => {
-    res.send("Put user API called");
-    // res.send({firstname: "John", lastname: "Doe"});
-});
-
-app.patch("/user", (req,res) => {
-    res.send("Patch user API called");
-    // res.send({firstname: "John", lastname: "Doe"});
-});
-
-app.delete("/user", (req,res) => {
-    res.send("Delete user API called");
-    // res.send({firstname: "John", lastname: "Doe"});
-});
-
-// use - This will match all the HTTP method API to /test
-app.use("/test",(req,res) => {
-    res.send( "Hello from server"); // Request handler
-});
-
-// app.use("/",(req,res) => {
-//     res.send( "Hello from dashboard"); // Request handler
-// });
-
 
 app.listen(3000, () =>{
     console.log("Server is successfully running on port 3000");
